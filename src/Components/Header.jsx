7355eb4baf0e988a66cc189c1409dfe3d22452ab
Joben <jjvillodrez@gmail.com>
2024-08-16 02:25:45 +0800

    final visitor page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Header() {
   const [] = useState("");
   return (
     <>
       <header>
-        <h1>This is a heading</h1>
+        <div>
+          <h1>Welcome!</h1>
+        </div>
+        <div className="socials">
+          <button>
+            <a href="https://www.linkedin.com/in/joben-villodrez/">
+              <LinkedInIcon />
+            </a>
+          </button>
+          <button>
+            <a href="https://github.com/jobendev">
+              <GitHubIcon />
+            </a>
+          </button>
+        </div>
       </header>
     </>
   );
