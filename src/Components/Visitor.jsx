24e5ab17865f0a4f2eb84f7657c228e665948c77
Joben <jjvillodrez@gmail.com>
2024-08-16 05:54:20 +0800

    final landing

--- src/Components/Visitor.jsx
+++ src/Components/Visitor.jsx
@@ -3,20 +3,25 @@ import Button from "@mui/material/Button";
 
 function Visitor(props) {
   return (
-    <div className="welcome">
-      <h1>
-      Thanks for visiting! I'm Joben,
-      </h1>
-      <p>what's your name?</p>
-      <div className="inputsubmit">
-        <input
-          className="input"
-          type="text"
-          placeholder="Please write your name"
-        />
-        <Button size="small">
-          next
-        </Button>
+    <div className="visit-container">
+      <div className="welcome">
+        <h1>
+          Thanks for visiting! I'm Joben a Scrum Master and a Full-stack Web
+          Developer,
+        </h1>
+        <p>what's your name?</p>
+        <div className="inputsubmit">
+          <input
+            className="input"
+            type="text"
+            placeholder="Please write your name"
+          />
+          <Button size="small">next</Button>
+        </div>
+      </div>
+
+      <div className="imagecontainer">
+        <img src="../src/assets/joben.png" alt="" />
       </div>
     </div>
   );
