8f90aa2bb28bedacd530903a71c4f24e893cdfbe
Joben <jjvillodrez@gmail.com>
2024-08-20 00:12:32 +0800

    added styling and tech stack

--- src/Components/Techstack.jsx
+++ src/Components/Techstack.jsx
@@ -1,45 +1,214 @@
 function Techstack() {
   return (
-    <div >
-      <h1>My Tech Stack</h1>
+    <div className="tech">
+      <h2>My Tech Stack</h2>
 
       <div className="techstackmain">
         <div className="cards">
-          <h2>Front-end</h2>
-          <ul>
-            <li>React JS</li>
-            <li>Vite</li>
-            <li>Bootstrap</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-            <li>jQuery</li>
-            <li>EJS Templating</li>
+          <div className="cardh2">
+            <h3>Front-end</h3>
+            <hr />
+          </div>
+
+          <ul className="ulflex">
+            <li>
+              {" "}
+              <img
+                className="stack-logo"
+                src="../src/assets/react.svg"
+                alt="react-logo"
+              />
+              React JS
+            </li>
+            <li>
+              {" "}
+              <img
+                className="stack-logo"
+                src="../src/assets/vite.svg"
+                alt="vite-logo"
+              />
+              Vite
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/bootstrap-fill-svgrepo-com.svg"
+                alt="bootstrap-logo"
+              />
+              Bootstrap
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/html-5-svgrepo-com.svg"
+                alt="html-logo"
+              />
+              HTML
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/css-3-svgrepo-com.svg"
+                alt="css"
+              />
+              CSS
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/javascript-svgrepo-com.svg"
+                alt="javascript"
+              />
+              Javascript
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/jquery-svgrepo-com.svg"
+                alt="jquery"
+              />
+              jQuery
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/ejs-svgrepo-com.svg"
+                alt="ejs"
+              />
+              EJS Templating
+            </li>
           </ul>
         </div>
 
         <div className="cards">
-          <h2>Back-end</h2>
-          <ul>
-            <li>NodeJS</li>
-            <li>Express</li>
-            <li>REST API</li>
-            <li>Authentication and Security handling</li>
-            <li>Oauth</li>
-            <li>PostgreSQL</li>
+          <div className="cardh2">
+            <h3>Back-end</h3>
+            <hr />
+          </div>
+
+          <ul className="ulflex">
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/nodejs-icon-svgrepo-com.svg"
+                alt="nodejs"
+              />
+              NodeJS
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/express-svgrepo-com.svg"
+                alt="express"
+              />
+              Express
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/rest-api-svgrepo-com.svg"
+                alt="restapi"
+              />
+              REST API
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/netiq-advanced-authentication-svgrepo-com.svg"
+                alt="authentication"
+              />
+              Authentication and Security handling
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/oauth-svgrepo-com.svg"
+                alt="oauth"
+              />
+              Oauth
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/postgresql-svgrepo-com.svg"
+                alt="postgresql"
+              />
+              PostgreSQL
+            </li>
           </ul>
         </div>
 
         <div className="cards">
-          <h2>Others</h2>
-          <ul>
-            <li>VS Code</li>
-            <li>Git</li>
-            <li>GitHub</li>
-            <li>Agile Methodologies</li>
-            <li>Scrum Framework</li>
-            <li>JIRA</li>
-            <li>Trello</li>
+          <div className="cardh2">
+            <h3>Others</h3>
+            <hr />
+          </div>
+
+          <ul className="ulflex">
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/vs-code-svgrepo-com.svg"
+                alt="vscode"
+              />
+              VS Code
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/git-svgrepo-com.svg"
+                alt="git"
+              />
+              Git
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/github-color-svgrepo-com.svg"
+                alt="github"
+              />
+              GitHub
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/agile-development-svgrepo-com.svg"
+                alt="agile"
+              />
+              Agile Methodologies
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/brand-scrum-svgrepo-com.svg"
+                alt="scrum"
+              />
+              Scrum Framework
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/jira-svgrepo-com.svg"
+                alt="jora"
+              />
+              JIRA
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/trello-color-svgrepo-com.svg"
+                alt="trello"
+              />
+              Trello
+            </li>
+            <li>
+              <img
+                className="stack-logo"
+                src="../src/assets/asana-svgrepo-com.svg"
+                alt="asana"
+              />
+              Asana
+            </li>
           </ul>
         </div>
       </div>
--- src/Components/Topintro.jsx
+++ src/Components/Topintro.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 function Topintro() {
   return (
-      <div className="topintro">
-          <img src="../src/assets/workout.png" alt="" />
+    <div className="topintro">
+      <img src="../src/assets/workout.png" alt="" />
       <h1>I'm Joben Villodrez</h1>
-      <h2><LocationOnIcon/>Palawan, Philippines</h2>
+      <h2>
+        <LocationOnIcon />
+        Palawan, Philippines
+      </h2>
       <p></p>
     </div>
   );
